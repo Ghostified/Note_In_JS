aab96e137df31109b2a167e1add34a7d70cef1ae
Ghostified <bransonallan@gmail.com>
2024-07-26 23:55:26 +0300

    added to functions to store notes on the browser

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const notesContainer = document.querySelector(".notes-container");
 const createButton = document.querySelector(".button");
 let notes = document.querySelectorAll(".input-Box");
 
+
+//function to store the notes on the browser
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+//Function to create the delete icon in the notes container
 createButton.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -10,3 +17,12 @@ createButton.addEventListener("click", ()=>{
     img.src ="images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 })
+
+//Event listenmer to delete a note when the the delete icon is clicked 
+notesContainer.addEventListener("click",function(e){
+    if (e.target.tagName == "IMG"){
+        e.target.parentElement.remove();
+    }
+})
+
+
