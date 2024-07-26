47db05094d8ab1f9661b636346533a8d24e8a338
Ghostified <bransonallan@gmail.com>
2024-07-27 00:15:35 +0300

    added a final event listener to add linebreaks to the notes app

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const notesContainer = document.querySelector(".notes-container");
 const createButton = document.querySelector(".button");
 let notes = document.querySelectorAll(".input-Box");
 
+//Function to restore and show notes content if browser is refreshed
+function showNotes() {
+    //if the notes are on the browser , we display them
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+//call the show notes function
+showNotes();
+
 
 //function to store the notes on the browser
 function updateStorage(){
@@ -18,21 +26,33 @@ createButton.addEventListener("click", ()=>{
     notesContainer.appendChild(inputBox).appendChild(img);
 })
 
-//Event listenmer to delete a note when the the delete icon is clicked 
+//Event listener to delete a note when the the delete icon is clicked 
 notesContainer.addEventListener("click",function(e){
-    if (e.target.tagName == "IMG"){
+    if (e.target.tagName === "IMG"){
         e.target.parentElement.remove();
 
         //update storage after deleting notes
         updateStorage();
     }
-    else if (e.target.tagName == "p"){
+    else if (e.target.tagName === "p"){
         notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
             notes.onkeyup = function(){
                 updateStorage();
             }
         })
+    }
+})
+
+//Event listener to add a line break
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+
+        //the event overides the enter default function
+        event.preventDefault();
+        updateStorage();
     }
 })
 
